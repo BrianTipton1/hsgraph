69cb7b948313a4ba89801aba844a911682cf3684
Brian Tipton <briantiptondev@gmail.com>
2023-09-02 03:50:06 -0500

    added command helper functions and fixing viztypes

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,19 @@
 {-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE LambdaCase #-}
 
 module Main where
 
-import GHC.Real (reduce)
-import System.Directory (doesDirectoryExist, doesFileExist)
-import System.Environment (getArgs)
-import System.Exit (ExitCode (ExitFailure), exitFailure, exitWith)
-import System.FilePath.Posix (takeBaseName, takeDirectory)
+import Control.Exception (Exception)
+import Control.Monad (filterM)
+import Data.Char (isAlpha, isDigit, toLower)
+import Data.List (partition)
+import Data.List.NonEmpty (fromList, toList)
+import System.Directory (createDirectory, createDirectoryIfMissing, doesDirectoryExist, doesFileExist, listDirectory)
+import System.Environment (getArgs, getEnv)
+import System.Exit (ExitCode (ExitFailure, ExitSuccess), exitFailure, exitWith)
+import System.FilePath.Posix (takeBaseName, takeDirectory, (</>))
+import System.Process (readCreateProcessWithExitCode, readProcess, readProcessWithExitCode)
+import Text.Printf (errorBadArgument)
 
 -- Helpers
 wrapStrInDoubleQuote :: String -> String
@@ -14,7 +21,72 @@ wrapStrInDoubleQuote s = "\"" ++ s ++ "\""
 redifyString :: String -> String
 redifyString s = "\ESC[31m" ++ s ++ "\ESC[0m"
 
+unique :: Eq a => [a] -> [a]
+unique [] = []
+unique (x : xs) = x : unique (filter (/= x) xs)
+
 -- End Helpers Section
+
+-- Command IO
+data CommandErrors
+    = UUIDNotFound String
+    | DotNotFound String
+    | ConvertNotFound String
+
+data Command
+    = UUIDgen
+    | Dot
+    | Convert
+
+instance Show Command where
+    show :: Command -> String
+    show UUIDgen = "uuidgen"
+    show Dot = "dot"
+    show Convert = "convert"
+
+unwrapErr :: CommandErrors -> String
+unwrapErr (UUIDNotFound e) = e
+unwrapErr (DotNotFound e) = e
+unwrapErr (ConvertNotFound e) = e
+
+commandToErr :: Command -> String -> CommandErrors
+commandToErr =
+    \case
+        UUIDgen -> UUIDNotFound
+        Dot -> DotNotFound
+        Convert -> ConvertNotFound
+
+commandBuilder :: Command -> IO b -> IO b
+commandBuilder cmd action = do
+    maybeGenerate <- maybeCommandErr cmd
+    case maybeGenerate of
+        Nothing -> action
+        Just e -> error $ unwrapErr e
+
+uuidgen :: IO String
+uuidgen = commandBuilder UUIDgen $ do
+    uuid <- init <$> readProcess (show UUIDgen) [] ""
+    let removedHyphens = filter (/= '-') uuid
+    let (alpha, numeric) = partition isAlpha removedHyphens
+    let newUUID = alpha ++ numeric
+    return newUUID
+
+dot :: String -> String -> IO ExitCode
+dot fileName fileType = commandBuilder Dot $ do
+    (ec, _, _) <- readProcessWithExitCode (show Dot) ["-Tpng", "-Gdpi=300", fileName, "-o", fileName ++ ".png"] ""
+    return ec
+
+maybeCommandErr :: Command -> IO (Maybe CommandErrors)
+maybeCommandErr cmd = do
+    let strCmd = show cmd
+    (ec, _, _) <- readProcessWithExitCode "which" [strCmd] ""
+    if ec == ExitSuccess
+        then return Nothing
+        else do
+            let err = commandToErr cmd
+            return $ Just (err $ redifyString $ "Error command " ++ wrapStrInDoubleQuote strCmd ++ " not found ...")
+
+-- End Command IO Section
 
 -- Custom Types relating to the assignment
 newtype Distance = Distance Integer
@@ -64,13 +136,35 @@ instance Show Node where
 
 --- End formatting section
 
---- Parsing of the files to custom types
+-- File IO
 fileToLines :: FilePath -> IO [String]
 fileToLines path = do
     fileContents <- readFile path
     let allLines = lines fileContents
     return $ tail allLines
 
+createOutDirStructure :: String -> IO ()
+createOutDirStructure graphFileName = do
+    pwd <- getEnv "PWD"
+    fileExists <- doesFileExist (pwd </> results)
+    if fileExists
+        then error $ redifyString "A regular file exists at $PWD/results please move this to continue ..."
+        else
+            mapM_
+                (dirStructureBuilder pwd)
+                [ "intermediteImages"
+                , "intermediateGraphVizFiles"
+                , "reports"
+                , "finalImages"
+                ]
+  where
+    results = "results"
+    dirStructureBuilder pwd child =
+        createDirectoryIfMissing True (pwd </> results </> graphFileName </> child)
+
+-- End File IO
+
+--- Parsing of the files to custom types
 getEdges :: [Edge] -> [String] -> [Edge]
 getEdges acc list =
     if even $ length list
@@ -78,9 +172,9 @@ getEdges acc list =
             (x : y : xs) ->
                 getEdges (Edge (Label x, Distance $ read y :: Distance) : acc) xs
             _ -> acc
-        else error "Incorrect dims when parsing edges"
+        else errorBadArgument $ redifyString "Incorrect dims when parsing edges"
 
-lineToNode :: [Char] -> Node
+lineToNode :: String -> Node
 lineToNode line =
     Node
         { label = Label node
@@ -110,7 +204,7 @@ pathToGraph path = linesToGraph path $ fileToLines path
 
 --- End parsing section
 
--- Extra Credit Graphviz
+-- Extra Credit GraphViz
 data VizColor
     = VizBlue
     | VizRed
@@ -121,7 +215,7 @@ vizColorToString :: VizColor -> String
 vizColorToString VizBlue = wrapStrInDoubleQuote "blue"
 vizColorToString VizRed = wrapStrInDoubleQuote "red"
 vizColorToString VizGreen = wrapStrInDoubleQuote "green"
-vizColorToString VizBlack = wrapStrInDoubleQuote "green"
+vizColorToString VizBlack = wrapStrInDoubleQuote "black"
 
 data VizEdgeType
     = VizEdgeSolid
@@ -147,9 +241,21 @@ vizArrowTypeToString VizArrowCurve = wrapStrInDoubleQuote "curve"
 vizArrowTypeToString VizArrowICurve = wrapStrInDoubleQuote "icurve"
 vizArrowTypeToString VizArrowVee = wrapStrInDoubleQuote "vee"
 
+data VizNodeProps = VizNodeProps
+    { width :: String
+    , height :: String
+    , vizPropLabel :: String
+    , style :: String
+    }
+
+data VizNode = VizNode
+    { vizId :: String
+    , props :: [VizNodeProps]
+    }
+
 data VizEdge = VizEdge
-    { vizSelf :: String
-    , vizOther :: String
+    { vizSelf :: VizNode
+    , vizOther :: VizNode
     , vizArrowType :: VizArrowType
     , vizArrow :: String
     , vizEdgeType :: VizEdgeType
@@ -163,7 +269,11 @@ vizEdgeToStringBuilder
         { vizArrowType = arrowType
         , vizEdgeType = edge
         , vizDistance = vizDistance
-        , vizSelf = self
+        , vizSelf =
+            VizNode
+                { vizId = id
+                , props = props
+                }
         , vizOther = other
         , vizColor = color
         , vizArrow = arrow
@@ -178,11 +288,10 @@ vizEdgeToStringBuilder
             ++ "arrowhead="
             ++ vizArrowTypeToString arrowType
             ++ "];\n\t"
-            ++ show self
+            -- ++ show self
             ++ " "
             ++ arrow
             ++ " "
-            ++ show other
 
 data VizType = VizDiGraph | VizGraph
 
@@ -202,6 +311,7 @@ data VizImage = VizImage
     { t :: VizType
     , edges :: [VizEdge]
     , name :: String
+    , path :: FilePath
     }
 instance Show VizImage where
     show :: VizImage -> String
@@ -217,16 +327,23 @@ instance Show VizImage where
 writeGraphViz :: FilePath -> VizImage -> IO ()
 writeGraphViz path image = writeFile path (show image)
 
+graphToVizImage :: Graph -> VizImage
+graphToVizImage
+    Graph
+        { fileName = fileName
+        , nodes = nodes
+        } = VizImage{}
+
 -- End Grapviz Section
 
 -- Command Line Section
 helpScreen :: String -> String
 helpScreen e =
     unlines
-        [ "Usage: hsgraph [OPTIONS] FILE"
+        [ "Usage: hsgraph [OPTIONS] (FILE|DIR)*"
         , ""
         , "Description:"
-        , "  hsgraph is a command-line utility for parsing and analyzing graphs. It operates on file(s) or multiple files within a directory. Optionally, it can also generate GraphViz graphs."
+        , "  hsgraph is a command-line utility for parsing and analyzing graphs. It operates on file(s) or multiple files within a directory(s). Optionally, it can also generate GraphViz graphs."
         , ""
         , "Options:"
         , "  -h, --help"
@@ -252,20 +369,32 @@ helpScreen e =
         , "  hsgraph myfile.txt -g --no-gif"
         , ""
         , "Note:"
-        , "  Ensure that the file or directory only contains files that hsgraph can parse."
+        , "  Ensure that the file(s) or directory(s) supplied only contains files that hsgraph can parse."
         , "  The -ng/--no-gif option can only be used in conjunction with the -g/--graph option."
         , e
         ]
 
--- To print the help screen, you can use:
--- putStrLn helpScreen
-data CommandLineOps
-    = CmdHelp
-    | CmdDirectoryPath FilePath
+data CmdPath
+    = CmdDirectoryPath {getFilePath :: FilePath}
+    | CmdFilePath {getFilePath :: FilePath}
+    deriving (Eq, Show)
+
+data CommandLineOption
+    = CmdPath CmdPath
+    | CmdHelp
     | CmdGraph
     | CmdNoGif
-    | CmdFilePath FilePath
     deriving (Eq, Show)
+
+mapCmdPathToPath :: [CommandLineOption] -> [FilePath]
+mapCmdPathToPath =
+    map
+        ( ( \case
+                CmdDirectoryPath p -> p
+                CmdFilePath p -> p
+          )
+            . (\(CmdPath fp) -> fp)
+        )
 
 cmdHelpList :: [String]
 cmdHelpList = ["-h", "--help"]
@@ -274,7 +403,7 @@ cmdGraphList = ["-g", "--graph"]
 cmdNoGifList :: [String]
 cmdNoGifList = ["-ng", "--no-gif"]
 
-strToOps :: String -> IO CommandLineOps
+strToOps :: String -> IO CommandLineOption
 strToOps s
     | s `elem` cmdHelpList = return CmdHelp
     | s `elem` cmdGraphList = return CmdGraph
@@ -283,22 +412,22 @@ strToOps s
         pathExists <- doesFileExist s
         directory <- doesDirectoryExist s
         case (pathExists, directory) of
-            (True, False) -> return $ CmdFilePath s
-            (False, True) -> return $ CmdDirectoryPath s
+            (True, False) -> return $ CmdPath $ CmdFilePath s
+            (False, True) -> return $ CmdPath $ CmdDirectoryPath s
             _ -> return CmdHelp
 
-badOpCombinations :: [CommandLineOps] -> IO ()
-badOpCombinations ops
-    | containsHelp ops = do
+checkOps :: [CommandLineOption] -> IO [CommandLineOption]
+checkOps ops
+    | containsHelp ops || null ops = do
         putStrLn $ helpScreen ""
         exitWith $ ExitFailure 1
     | isGifErr ops = do
         putStrLn $ helpScreen gifErrMsg
         exitWith $ ExitFailure 1
-    | otherwise = return ()
+    | otherwise = return ops
   where
     containsHelp = elem CmdHelp
-    isGifErr :: [CommandLineOps] -> Bool
+    isGifErr :: [CommandLineOption] -> Bool
     isGifErr xs = elem CmdNoGif xs && notElem CmdGraph xs
     gifErrMsg =
         redifyString $
@@ -307,9 +436,54 @@ badOpCombinations ops
                 ++ " option to use "
                 ++ wrapStrInDoubleQuote "-ng/--no-gif"
 
+_allDirFiles :: [FilePath] -> [FilePath] -> IO [FilePath]
+_allDirFiles acc dirs =
+    case dirs of
+        [] -> return acc
+        [x] ->
+            ( \_ -> do
+                allContents <- listDirectory x
+                files <- dirFilter allContents
+                return $ acc ++ map (x </>) files
+            )
+                ()
+        (x : xs) ->
+            ( \_ -> do
+                allContents <- listDirectory x
+                files <- dirFilter allContents
+                _allDirFiles (acc ++ map (x </>) files) xs
+            )
+                ()
+  where
+    isFileM :: FilePath -> IO Bool
+    isFileM path = do
+        result <- doesDirectoryExist path
+        return (not result)
+    dirFilter :: [FilePath] -> IO [FilePath]
+    dirFilter = filterM isFileM
+
+allDirFiles :: [FilePath] -> IO [FilePath]
+allDirFiles = _allDirFiles []
+
+isCmdDirectory :: CommandLineOption -> Bool
+isCmdDirectory x =
+    case x of
+        (CmdPath (CmdDirectoryPath x)) -> True
+        _ -> False
+
+isCmdFile :: CommandLineOption -> Bool
+isCmdFile x =
+    case x of
+        (CmdPath (CmdFilePath x)) -> True
+        _ -> False
+
+--- End Command Line Section
+
 main :: IO ()
 main = do
     args <- getArgs
-    ops <- sequence $ strToOps <$> args
-    -- graph <- pathToGraph $ head args
-    badOpCombinations ops
+    ops <- sequence (strToOps <$> args) >>= checkOps
+    let dirs = mapCmdPathToPath (filter isCmdDirectory ops)
+    readDirFiles <- allDirFiles dirs
+    let allFiles = mapCmdPathToPath (filter isCmdFile ops) ++ readDirFiles
+    print $ unique allFiles
