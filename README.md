c85e2ee89699e3421d002327dd71b8c84c599015
Brian Tipton <briantiptondev@gmail.com>
2023-09-03 19:00:09 -0500

    dijkstra impl dijkstra timer function pdflatex cmd added latex table generator can now specify starting node via cmdline updated readme

--- README.md
+++ README.md
@@ -1,20 +1,86 @@
 # CS438 Search
 
+## About 
+CLI program written in haskell to compare graph algorithms performance and use cases. Written with no external libraries because cabal/stack package manager is not available for use in the assignment testing environment. 
+
 ##  Instructor building from the extracted zip
-> Shouldn't require any special compiler flags
+Shouldn't require any special compiler flags. All code is in the `Main.hs` file. The file is broken into comment blocks that wrap the code to outline  distinctually different areas of the program. The block with the comment "Course Work" is all the logic pertaining to where the base assignment requirements are (ie Dijkstras, BFS, DFS).
 ```bash
 ghc -o hsgraph ./app/Main.hs
 ```
 ## Instructor Usage
+- All commands needed for full generation are available and tested on home.cs.siue.edu
+- The following command will generate a single PDF report and graph(s) to accompany each file found/provided to the command
 ```bash
-./hsgraph "/path/to/my/(graphFile|graphFilledDir)"
+./hsgraph /path/to/my/(graphFile|graphFilledDir) /optionally/other/file(s) -g
 ```
 
 ### Chat GPT
  - [Transcript for generating the help text](https://chat.openai.com/share/9af8ee57-d54a-4ad2-a799-8bac8a9a7f88)
+ - [Transcript for generating latex table from custom table type](https://chat.openai.com/share/9ba74a22-018a-4302-abd3-81778c888b32)
+
+### Shell Command Dependencies
+- Report Generation
+    - `pdflatex`
+- Graph Generation
+    - GraphViz Package
+        - `dot` -> Builds a "prettier" graph but ignores the len attribute on edges
+        - `convert` -> Builds gifs from the images
+- Other Builtins/Standard
+    - `uuidgen`
+    - `which`
 
 ## Clone + Cabal Usage 
 ```bash
 git clone git@github.com:BrianTipton1/hsgraph.git
 cabal run hsgraph -- "/path/to/my/(graphFile|graphFilledDir)"
 ```
+
+### Help Text
+```bash
+./hsgraph --help
+```
+```
+Usage: hsgraph [OPTIONS] (FILE|DIR)*
+
+Description:
+  hsgraph is a command-line utility for parsing and analyzing graphs using Dijkstra's Algorithm, BFS and DFS. It operates on file(s) or multiple files within a directory(s). Optionally, it can also generate GraphViz graphs. The report and images are generated to $PWD/results
+
+Options:
+  -h, --help
+    Show this help message and exit.
+
+  -g, --graph
+    Generate VizGraphs for the selected file(s).
+
+  -ng, --no-gif
+    Do not generate GIFs from the image files. This option can only be used in conjunction with the -g/--graph option.
+
+  -s=2, --start-node=2
+    Optionally supply a node to start algorithms from
+
+Arguments:
+  FILE
+    Required. The path to a file(s) and or a directory(s) to be parsed.
+
+Examples:
+  hsgraph myfile.txt
+  hsgraph myfile.txt /path/to/directory
+  hsgraph /path/to/directory
+  hsgraph /path/to/directory -g
+  hsgraph myfile.txt -g
+  hsgraph /path/to/directory -g --no-gif
+  hsgraph myfile.txt -g --no-gif
+
+Note:
+  Ensure that the file(s) or directory(s) supplied only contains files that hsgraph can parse.
+  The -ng/--no-gif option can only be used in conjunction with the -g/--graph option.
+```
+
+### For future me if looking for libs/resources I found
+- Resources
+  - [graphviz](https://graphviz.org/)
+- Packages
+  - [cmdargs](https://hackage.haskell.org/package/cmdargs)
+  - [graphviz](https://hackage.haskell.org/package/graphviz)
+  - [HaTeX](https://hackage.haskell.org/package/HaTeX)
--- app/Main.hs
+++ app/Main.hs
@@ -3,15 +3,18 @@
 
 module Main where
 
-import Control.Exception (Exception)
+import Control.Exception (Exception, evaluate)
 import Control.Monad (filterM)
 import Data.Char (isAlpha, isDigit, toLower)
-import Data.List (foldl', partition)
+import Data.Either (fromRight, rights)
+import Data.List (delete, find, foldl', minimumBy, partition)
+import Data.Ord (comparing)
+import System.CPUTime (getCPUTime)
 import System.Directory (createDirectory, createDirectoryIfMissing, doesDirectoryExist, doesFileExist, listDirectory)
 import System.Environment (getArgs, getEnv)
 import System.Exit (ExitCode (ExitFailure, ExitSuccess), exitFailure, exitWith)
 import System.FilePath.Posix (takeBaseName, takeDirectory, (</>))
-import System.Process (readCreateProcessWithExitCode, readProcess, readProcessWithExitCode)
+import System.Process (readProcess, readProcessWithExitCode)
 import Text.Printf (errorBadArgument)
 
 -- Helpers
@@ -24,6 +27,30 @@ unique :: Eq a => [a] -> [a]
 unique [] = []
 unique (x : xs) = x : unique (filter (/= x) xs)
 
+split :: Char -> String -> [String]
+split _ "" = [""]
+split delimiter (x : xs)
+    | x == delimiter = "" : rest
+    | otherwise = (x : head rest) : tail rest
+  where
+    rest = split delimiter xs
+
+_timer :: IO a -> IO (a, String)
+_timer f = do
+    startTime <- getCPUTime
+    result <- f
+    endTime <- getCPUTime
+    let diffPico = fromIntegral (endTime - startTime)
+    let timeStr
+            | diffPico < 1000 = "$$" ++ show diffPico ++ " ps$$"
+            | diffPico < 10 ^ 6 = "$$" ++ show (diffPico / 10 ^ 3) ++ " ns$$"
+            | diffPico < 10 ^ 9 = "$$" ++ show (diffPico / 10 ^ 6) ++ " \\mu s$$"
+            | otherwise = "$$" ++ show (diffPico / 10 ^ 9) ++ " ms$$"
+    return (result, timeStr)
+
+timeF :: a -> IO (a, String)
+timeF f = _timer (evaluate f)
+
 -- End Helpers Section
 
 -- Command IO
@@ -31,22 +58,26 @@ data CommandErrors
     = UUIDNotFound String
     | DotNotFound String
     | ConvertNotFound String
+    | PdfLatexNotFound String
 
 data Command
     = UUIDgen
     | Dot
     | Convert
+    | PdfLatex
 
 instance Show Command where
     show :: Command -> String
     show UUIDgen = "uuidgen"
     show Dot = "dot"
     show Convert = "convert"
+    show PdfLatex = "pdflatex"
 
 unwrapErr :: CommandErrors -> String
 unwrapErr (UUIDNotFound e) = e
 unwrapErr (DotNotFound e) = e
 unwrapErr (ConvertNotFound e) = e
+unwrapErr (PdfLatexNotFound e) = e
 
 commandToErr :: Command -> String -> CommandErrors
 commandToErr =
@@ -54,6 +85,7 @@ commandToErr =
         UUIDgen -> UUIDNotFound
         Dot -> DotNotFound
         Convert -> ConvertNotFound
+        PdfLatex -> PdfLatexNotFound
 
 commandBuilder :: Command -> IO b -> IO b
 commandBuilder cmd action = do
@@ -62,12 +94,32 @@ commandBuilder cmd action = do
         Nothing -> action
         Just e -> error $ unwrapErr e
 
-uuidgen :: IO String
+data LatexOutFormat = Pdf | Png
+instance Show LatexOutFormat where
+    show :: LatexOutFormat -> String
+    show Pdf = "pdf"
+    show Png = "png"
+
+pdfLatex :: FilePath -> FilePath -> LatexOutFormat -> IO ExitCode
+pdfLatex texPath outDir fmt = commandBuilder PdfLatex $ do
+    (ec, _, _) <-
+        readProcessWithExitCode
+            (show PdfLatex)
+            ["-output-directory=" ++ outDir, "-output-format=" ++ show fmt]
+            ""
+    return ec
+
+newtype UUID = UUID String
+instance Show UUID where
+    show :: UUID -> String
+    show (UUID uuid) = uuid
+
+uuidgen :: IO UUID
 uuidgen = commandBuilder UUIDgen $ do
     uuid <- init <$> readProcess (show UUIDgen) [] ""
     let removedHyphens = filter (/= '-') uuid
     let (alpha, numeric) = partition isAlpha removedHyphens
-    return $ alpha ++ numeric
+    return $ UUID $ alpha ++ numeric
 
 dot :: String -> String -> IO ExitCode
 dot fileName fileType = commandBuilder Dot $ do
@@ -95,26 +147,30 @@ maybeCommandErr cmd = do
 
 -- End Command IO Section
 
--- Custom Types relating to the assignment
-newtype Distance = Distance Integer
-newtype Label = Label String
-newtype Edge = Edge (Label, Distance) deriving (Show)
+-- Report/Latex Stuff
 
+generateLatexTable :: DijkstraTable -> Label -> String -> String
+generateLatexTable (DijkstraTable rows) (Label startLabel) filename =
+    unlines
+        ( [ "\\begin{table}[h]"
+          , "  \\centering"
+          , "  \\caption{Distances from Node " ++ startLabel ++ " to all other nodes using Dijkstra's Algorithm from the file " ++ filename ++ "}"
+          , "  \\begin{tabular}{|c|c|}"
+          , "    \\hline"
+          , "    Node & Distance \\\\"
+          , "    \\hline"
+          ]
+            ++ rowStrings
+            ++ [ "    \\hline"
+               , "  \\end{tabular}"
+               , "\\end{table}"
+               ]
+        )
+  where
+    rowStrings = map rowToLatex rows
+    rowToLatex (DijkstraRow (Label lbl, Distance dist)) = "    " ++ lbl ++ " & " ++ show dist ++ " \\\\ \\hline"
 
-data Graph = Graph
-    { nodes :: [Node]
-    , nNodes :: Int
-    , fileName :: String
-    , directory :: FilePath
-    }
-    deriving (Show)
-
-data Node = Node
-    { label :: Label
-    , adjacent :: [Edge]
-    }
-
---- End custom types section
+---
 
 -- Subpar formatting for Pretty Printing of Assignment Types
 instance Show Label where
@@ -155,7 +211,7 @@ createOutDirStructure graphFileName = do
                 (dirStructureBuilder pwd)
                 [ "intermediteImages"
                 , "intermediateGraphVizFiles"
-                , "reports"
+                , "report"
                 , "finalImages"
                 ]
   where
@@ -322,10 +378,10 @@ instance Show VizImage where
         nodePropertyDefs =
             concatNlTab $
                 map
-                    (\VizNode{vizId = vizId, nodeProps = props} -> vizId ++ " " ++ show props ++ "\n\t")
+                    (\VizNode{vizId = vizId, nodeProps = props} -> vizId ++ " " ++ show props ++ "\t")
                     (edgesToUniqueNodes edges)
-        edgeStrs = concatNlTab $ map (\x -> show x ++ "\n\t") edges
-        concatNlTab = foldl' (++) "\n\t"
+        edgeStrs = concatNlTab $ map show edges
+        concatNlTab = foldl' (\x y -> x ++ "\n\t" ++ y) ""
 
 edgesToAllNodes :: [VizEdge] -> [VizNode]
 edgesToAllNodes =
@@ -358,32 +414,6 @@ writeGraphViz :: VizImage -> IO ()
 writeGraphViz image =
     writeFile (path image) (show image)
 
-unDirGraphToVizImage :: FilePath -> Graph -> VizImage
-unDirGraphToVizImage
-    dir
-    Graph
-        { fileName = fileName
-        , nodes = nodes
-        } =
-        VizImage
-            { t = VizGraph
-            , edges = unique $ mapNodesToVizEdges nodes
-            , name = fileName
-            , path = dir </> fileName
-            }
-
-mapNodesToVizEdges :: [Node] -> [VizEdge]
-mapNodesToVizEdges =
-    map
-        ( \Node
-            { label = label
-            , adjacent = adjacent
-            } ->
-                VizEdge
-                    {
-                    }
-        )
-
 -- End Grapviz Section
 
 -- Command Line Section
@@ -393,7 +423,7 @@ helpScreen e =
         [ "Usage: hsgraph [OPTIONS] (FILE|DIR)*"
         , ""
         , "Description:"
-        , "  hsgraph is a command-line utility for parsing and analyzing graphs. It operates on file(s) or multiple files within a directory(s). Optionally, it can also generate GraphViz graphs."
+        , "    hsgraph is a command-line utility for parsing and analyzing graphs using Dijkstra's Algorithm, BFS and DFS. It operates on file(s) or multiple files within a directory(s). Optionally, it can also generate GraphViz graphs. The report and images are generated to $PWD/results"
         , ""
         , "Options:"
         , "  -h, --help"
@@ -404,6 +434,9 @@ helpScreen e =
         , ""
         , "  -ng, --no-gif"
         , "    Do not generate GIFs from the image files. This option can only be used in conjunction with the -g/--graph option."
+        , ""
+        , "  -s=2, --start-node=2"
+        , "    Optionally supply a node to start algorithms from"
         , ""
         , "Arguments:"
         , "  FILE"
@@ -434,6 +467,7 @@ data CommandLineOption
     | CmdHelp
     | CmdGraph
     | CmdNoGif
+    | CmdStartNode String
     deriving (Eq, Show)
 
 mapCmdPathToPath :: [CommandLineOption] -> [FilePath]
@@ -452,12 +486,15 @@ cmdGraphList :: [String]
 cmdGraphList = ["-g", "--graph"]
 cmdNoGifList :: [String]
 cmdNoGifList = ["-ng", "--no-gif"]
+cmdStartNode :: String -> [String]
+cmdStartNode s = (++ s) <$> ["-s=", "--start-node="]
 
 strToOps :: String -> IO CommandLineOption
 strToOps s
     | s `elem` cmdHelpList = return CmdHelp
     | s `elem` cmdGraphList = return CmdGraph
     | s `elem` cmdNoGifList = return CmdNoGif
+    | s `elem` cmdStartNode (last startNode) = return $ CmdStartNode (last startNode)
     | otherwise = do
         pathExists <- doesFileExist s
         directory <- doesDirectoryExist s
@@ -465,6 +502,8 @@ strToOps s
             (True, False) -> return $ CmdPath $ CmdFilePath s
             (False, True) -> return $ CmdPath $ CmdDirectoryPath s
             _ -> return CmdHelp
+  where
+    startNode = split '=' s
 
 checkOps :: [CommandLineOption] -> IO [CommandLineOption]
 checkOps ops
@@ -527,13 +566,105 @@ isCmdFile x =
         (CmdPath (CmdFilePath x)) -> True
         _ -> False
 
+getStartingLabel :: [CommandLineOption] -> Label
+getStartingLabel cmds =
+    case length startNodes of
+        0 -> Label "1"
+        1 -> Label $ head startNodes
+        _ -> error $ redifyString "Only one start node can be specified ..."
+  where
+    startNodes = [i | CmdStartNode i <- cmds]
+
 --- End Command Line Section
+
+-----------------Course Work------------------
+
+-- Custom Types relating to the assignment
+newtype Distance
+    = Distance Int
+    deriving (Eq, Ord)
+newtype Label
+    = Label String
+    deriving (Eq, Ord)
+newtype Edge = Edge (Label, Distance) deriving (Show)
+
+data Graph = Graph
+    { nodes :: [Node]
+    , nNodes :: Int
+    , fileName :: String
+    , directory :: FilePath
+    }
+    deriving (Show)
+
+data Node = Node
+    { label :: Label
+    , adjacent :: [Edge]
+    }
+
+--- End custom types section
+
+-- Dijkstra Specific
+newtype DijkstraRow = DijkstraRow (Label, Distance) deriving (Show, Eq)
+newtype DijkstraTable = DijkstraTable [DijkstraRow] deriving (Show)
+
+newtype From = From Label deriving (Show)
+newtype To = To Label deriving (Show)
+type TableState = DijkstraTable
+newtype Transition = Transition ((From, To), TableState) deriving (Show)
+type TransitionSpace = [Either Transition Transition]
+
+rowToTuple :: DijkstraRow -> (Label, Distance)
+rowToTuple (DijkstraRow (l, d)) = (l, d)
+
+unwrapRows :: DijkstraTable -> [DijkstraRow]
+unwrapRows (DijkstraTable rows) = rows
+
+tableToTuples :: DijkstraTable -> [(Label, Distance)]
+tableToTuples t = map rowToTuple $ unwrapRows t
+
+dijkstra :: Graph -> Label -> (DijkstraTable, TransitionSpace)
+dijkstra graph start =
+    let initial = DijkstraTable [DijkstraRow (label, if label == start then Distance 0 else Distance maxBound) | Node label _ <- nodes graph]
+     in dijkstra' graph [] (initial, [])
+
+dijkstra' :: Graph -> [Label] -> (DijkstraTable, TransitionSpace) -> (DijkstraTable, TransitionSpace)
+dijkstra' _ visited (table, space)
+    | all ((`elem` visited) . fst) (tableToTuples table) = (table, space)
+
+dijkstra' graph visited (table, space) =
+    let unvisited = filter (\(l, _) -> l `notElem` visited) (tableToTuples table)
+        (nextLabel, nextDistance) = minimumBy (comparing snd) unvisited
+        Just (Node _ adjacents) = find (\(Node label _) -> label == nextLabel) (nodes graph)
+        updatedDistances = foldl' (updateDistance nextLabel nextDistance) (table, space) adjacents
+     in dijkstra' graph (nextLabel : visited) updatedDistances
+
+updateDistance :: Label -> Distance -> (DijkstraTable, TransitionSpace) -> Edge -> (DijkstraTable, TransitionSpace)
+updateDistance current (Distance currentDistance) (distanceTable, space) (Edge (destination, Distance weight))
+    | newDistance < destinationDistance =
+        ( DijkstraTable $ DijkstraRow (destination, Distance newDistance) : delete (DijkstraRow (destination, Distance destinationDistance)) (unwrapRows distanceTable)
+        , Right (Transition ((From current, To destination), distanceTable)) : space
+        )
+    | otherwise =
+        ( distanceTable
+        , Left (Transition ((From current, To destination), distanceTable)) : space
+        )
+  where
+    Just (Distance destinationDistance) = lookup destination (tableToTuples distanceTable)
+    newDistance = currentDistance + weight
+
+-- End Dijkstra Stuff
+
+-------------- End Course Work Section ---------------
 
 main :: IO ()
 main = do
     args <- getArgs
     ops <- sequence (strToOps <$> args) >>= checkOps
+    let startLabel = getStartingLabel ops
     let dirs = mapCmdPathToPath (filter isCmdDirectory ops)
     readDirFiles <- allDirFiles dirs
     let allFiles = mapCmdPathToPath (filter isCmdFile ops) ++ readDirFiles
-    print $ unique allFiles
+    graphs <- mapM pathToGraph $ unique allFiles
+    ((fin, res), time) <- timeF $ dijkstra (head graphs) startLabel
+    -- putStrLn $ generateLatexTable fin startLabel (fileName $ head graphs)
+    print fin
